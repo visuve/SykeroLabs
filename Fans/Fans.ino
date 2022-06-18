19e715e9ab94d6a4779fa4655336c7a9b6345d59
visuve <veli-matti.visuri@iki.fi>
2022-06-18 21:27:52 +0300

    Measure fan RPM

--- Fans/Fans.ino
+++ Fans/Fans.ino
@@ -2,12 +2,14 @@
   Control fan speed using PWM and a DS18B20 temperature sensor.
 */
 
-
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-constexpr uint8_t TEMPERATURE_PIN = 2;
+constexpr uint8_t RPM_INPUT_PIN = 2;
 constexpr uint8_t PWM_OUTPUT_PIN = 3;
+constexpr uint8_t TEMPERATURE_PIN = 9;
+
+volatile uint32_t revolutions = 0;
 
 OneWire oneWire(TEMPERATURE_PIN);
 DallasTemperature sensors(&oneWire);
@@ -15,12 +17,25 @@ DallasTemperature sensors(&oneWire);
 void setup() { 
   pinMode(PWM_OUTPUT_PIN, OUTPUT);
 
+  const int32_t interruptPin = digitalPinToInterrupt(RPM_INPUT_PIN);
+  attachInterrupt(interruptPin, revolutionCounter, RISING);
+
   Serial.begin(9600);
   sensors.begin();
 
   while (!Serial) {
     delay(10);
   }
+}
+
+void revolutionCounter() {
+  ++revolutions;
+}
+
+uint32_t measureRpm() {
+  revolutions = 0;
+  delay(1000);
+  return revolutions * 30;
 }
 
 uint8_t pulseWidthFromTemperature(float temperature) {
@@ -41,16 +56,18 @@ uint8_t pulseWidthFromTemperature(float temperature) {
 }
 
 void loop() {
-  delay(2000);
+  delay(1000);
 
   sensors.requestTemperatures();
   float temperature = sensors.getTempCByIndex(0);
   uint8_t pulseWidth = pulseWidthFromTemperature(temperature);
+  uint32_t rpm = measureRpm();
+  analogWrite(PWM_OUTPUT_PIN, pulseWidth);
 
   Serial.print("TMP=");
   Serial.print(temperature);
   Serial.print(" PWM=");
-  Serial.println(pulseWidth);
-
-  analogWrite(PWM_OUTPUT_PIN, pulseWidth);
+  Serial.print(pulseWidth);
+  Serial.print(" RPM=");
+  Serial.println(rpm);
 }
